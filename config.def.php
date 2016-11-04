26972714550e2d7ef6c3add70226e7c2c5a94a14
Jevgenij Volosatov <fformula@gmail.com>
2016-11-04 16:16:13 +0200

    def config

---
+++ config.def.php
@@ -0,0 +1,13 @@
+<?php
+    define ("ROOT", "");
+    define ("DATA_DEFAULT_MODULE", "help");
+    define ("DATA_DEFAULT_ACTION", "index");
+    define ("API_PREFIX", "api_");
+    define ("DB_ENGINE", "mysql");
+    define ("DB_HOST", "localhost");
+    define ("DB_USER", "root");
+    define ("DB_PASS", "qwas");
+    define ("DB_BASE", "drivers");
+    define ("DB_PORT", "3306");
+
+    define ("SMARTY_TEMPLATES_DIR", "templates");
