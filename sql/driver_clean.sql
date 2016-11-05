0a5ab9f2aae517c377959ca47fac32ecb0e62e00
justangel <admin@just-gaming.ru>
2016-11-05 23:26:45 +0300

    рефакторинг driver_clean.sql файла (пустая структура БД, основа) удаление одноимённых таблиц происходит в обратной последовательности, чтобы небыло возможных ошибок особенностей структуры БД добавление таблиц происходит в последователности связки таблиц (если её нарушить, могут быть ошибки)
    Signed-off-by: justangel <admin@just-gaming.ru>

--- sql/driver_clean.sql
+++ sql/driver_clean.sql
@@ -1,54 +1,25 @@
--- MySQL dump 10.13  Distrib 5.6.28, for Win64 (x86_64)
---
--- Host: localhost    Database: drivers
--- ------------------------------------------------------
--- Server version	5.6.28-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `docs`
---
-
+-- ----------------------------
+-- Drop tables in correct sequence if exists
+-- ----------------------------
 DROP TABLE IF EXISTS `docs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `docs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `driver_id` int(11) DEFAULT NULL,
-  `filename` varchar(255) DEFAULT NULL,
-  `info` text COMMENT 'Тип документа и комментарий',
-  PRIMARY KEY (`id`),
-  KEY `FK_docs_drivers` (`driver_id`),
-  CONSTRAINT `FK_docs_drivers` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Список прикреплённых файлов-документов';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `docs`
---
-
-LOCK TABLES `docs` WRITE;
-/*!40000 ALTER TABLE `docs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `docs` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `drivers`
---
-
 DROP TABLE IF EXISTS `drivers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+DROP TABLE IF EXISTS `users`;
+
+-- ----------------------------
+-- Table structure for `users`
+-- ----------------------------
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Список пользователей системы';
+
+-- ----------------------------
+-- Table structure for `drivers`
+-- ----------------------------
 CREATE TABLE `drivers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
@@ -64,53 +35,17 @@ CREATE TABLE `drivers` (
   PRIMARY KEY (`id`),
   KEY `FK_drivers_users` (`user_id`),
   CONSTRAINT `FK_drivers_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Список водителей';
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Список водителей';
 
---
--- Dumping data for table `drivers`
---
-
-LOCK TABLES `drivers` WRITE;
-/*!40000 ALTER TABLE `drivers` DISABLE KEYS */;
-INSERT INTO `drivers` VALUES (1,NULL,'2016-11-04 12:13:19','2016-11-04 12:14:22','Bookin','Gena','X','LB','12345678',2,NULL);
-/*!40000 ALTER TABLE `drivers` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
+-- ----------------------------
+-- Table structure for `docs`
+-- ----------------------------
+CREATE TABLE `docs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='Список пользователей системы';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'John','formulist@gmail.com','qwas',2),(2,'Mike','mike@gmail.com','qwas',1),(3,'Admin','admin@gmail.com','qwas',2);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-11-04 15:50:32
+  `driver_id` int(11) DEFAULT NULL,
+  `filename` varchar(255) DEFAULT NULL,
+  `info` text COMMENT 'Тип документа и комментарий',
+  PRIMARY KEY (`id`),
+  KEY `FK_docs_drivers` (`driver_id`),
+  CONSTRAINT `FK_docs_drivers` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Список прикреплённых файлов-документов';
