d6cc28a69479516d4521fbd83c427e91b74894de
Jevgenij Volosatov <fformula@gmail.com>
2016-11-06 13:04:09 +0200

    Исправлен открываемый по умолчанию модуль
    Signed-off-by: Jevgenij Volosatov <fformula@gmail.com>

--- config.def.php
+++ config.def.php
@@ -1,7 +1,7 @@
 <?php
     define ("ROOT", "");
-    define ("DATA_DEFAULT_MODULE", "help");
-    define ("DATA_DEFAULT_ACTION", "index");
+    define ("DATA_DEFAULT_MODULE", "driver");
+    define ("DATA_DEFAULT_ACTION", "find");
     define ("API_PREFIX", "api_");
     define ("POST_SUFFIX", "_post");
     define ("DB_ENGINE", "mysql");
